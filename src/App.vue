731a1f27fa0933838fcc4d3f93d9068a601891df
jarvisyu <15824456718@163.com>
2020-03-16 21:53:29 +0800

    tabBar

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
 </template>
 
 <script>
-import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import MainTabBar from './components/content/mainTabbar/MainTabBar'
 
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
